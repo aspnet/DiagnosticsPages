f0fdd289ef4b72c3e566d4aabf9755edc2b95322
Hao Kung <haok@microsoft.com>
2016-02-29 14:34:19 -0800

    Update options ctors

--- src/Microsoft.AspNetCore.Diagnostics.EntityFrameworkCore/DatabaseErrorPageOptions.cs
+++ src/Microsoft.AspNetCore.Diagnostics.EntityFrameworkCore/DatabaseErrorPageOptions.cs
@@ -1,8 +1,10 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Diagnostics.EntityFrameworkCore;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
 
 namespace Microsoft.AspNetCore.Builder
 {
@@ -11,6 +13,26 @@ namespace Microsoft.AspNetCore.Builder
     /// </summary>
     public class DatabaseErrorPageOptions
     {
+        /// <summary>
+        /// Constructs a new DatabaseErrorPageOptions.
+        /// </summary>
+        public DatabaseErrorPageOptions() { }
+
+        /// <summary>
+        /// Constructs and configures a new DatabaseErrorPageOptions.
+        /// </summary>
+        /// <param name="configureOptions">The configuration actions that will be applied.</param>
+        public DatabaseErrorPageOptions(IEnumerable<IConfigureOptions<DatabaseErrorPageOptions>> configureOptions) : this()
+        {
+            if (configureOptions != null)
+            {
+                foreach (var configure in configureOptions)
+                {
+                    configure.Configure(this);
+                }
+            }
+        }
+
         /// <summary>
         /// Gets or sets the path that <see cref="MigrationsEndPointMiddleware"/> will listen
         /// for requests to execute migrations commands.
--- src/Microsoft.AspNetCore.Diagnostics.EntityFrameworkCore/MigrationsEndPointOptions.cs
+++ src/Microsoft.AspNetCore.Diagnostics.EntityFrameworkCore/MigrationsEndPointOptions.cs
@@ -1,8 +1,10 @@
 // Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Diagnostics.EntityFrameworkCore;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
 
 namespace Microsoft.AspNetCore.Builder
 {
@@ -11,6 +13,26 @@ namespace Microsoft.AspNetCore.Builder
     /// </summary>
     public class MigrationsEndPointOptions
     {
+        /// <summary>
+        /// Constructs a new MigrationsEndPointOptions.
+        /// </summary>
+        public MigrationsEndPointOptions() { }
+
+        /// <summary>
+        /// Constructs and configures a new MigrationsEndPointOptions.
+        /// </summary>
+        /// <param name="configureOptions">The configuration actions that will be applied.</param>
+        public MigrationsEndPointOptions(IEnumerable<IConfigureOptions<MigrationsEndPointOptions>> configureOptions) : this()
+        {
+            if (configureOptions != null)
+            {
+                foreach (var configure in configureOptions)
+                {
+                    configure.Configure(this);
+                }
+            }
+        }
+
         /// <summary>
         /// The default value for <see cref="Path"/>.
         /// </summary>
