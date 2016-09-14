cb7ac7978174eef495c65d13063cd3f3c4f40ce6
Mikael Mengistu <mimengis@microsoft.com>
2016-09-14 13:57:45 -0700

    Using doc comment from IHttpRequestFeature

--- src/Microsoft.AspNetCore.Diagnostics.Abstractions/IExceptionHandlerPathFeature.cs
+++ src/Microsoft.AspNetCore.Diagnostics.Abstractions/IExceptionHandlerPathFeature.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
-
 namespace Microsoft.AspNetCore.Diagnostics
 {
     /// <summary>
@@ -11,7 +9,8 @@ namespace Microsoft.AspNetCore.Diagnostics
     public interface IExceptionHandlerPathFeature : IExceptionHandlerFeature
     {
         /// <summary>
-        /// The path of request that caused the exception. The value is un-escaped.
+        /// The portion of the request path that identifies the requested resource. The value
+        /// is un-escaped.
         /// </summary>
         string Path { get; }
     }
